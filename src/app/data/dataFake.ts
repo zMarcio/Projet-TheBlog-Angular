897cc8d2d82212c23bdaeccfe6d8341548f64e1b
zMarcio <marciopaulolima21@edu.unifor.br>
2023-04-19 17:45:13 -0300

    Falta ultimo small-card colocar música no small-card de música e só isso

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -25,22 +25,22 @@ export const dataFake = [
     },
     {
         "id" : '2' ,
-        "photo":"aaa",
-        "title":"saiu novo filme",
+        "photo":"../assets/imgs/FortalezaPenta.png",
+        "title":"Fortaleza Esporte Clube",
                 
-        "description":'',
+        "description":'Fortaleza, uma paixão passada de pai para filho, de uma forma louca no início, que se transformou em amor ao longo do tempo.',
 
-        "description1":'',
+        "description1":'No início, com meus 7 ou 8 anos, eu ia ao estádio com meu pai, mesmo com o time não estando lá essas coisas. Eu não entendia o que estava acontecendo em termos de competição e tudo mais, mas gostava de ir pro estádio e, como todo moleque, o sonho era ser jogador profissional.',
 
-        "description2":'',
+        "description2":'Eu amava estar lá, mas infelizmente, nem tudo são flores. Lembro-me de um jogo, clássico (não me lembro ao certo, mas acho que era clássico), onde houve uma confusão no final do jogo. O Fortaleza perdeu e na saída teve um empurra-empurra. Como criança, fiquei assustado e, desde esse dia, deixei de lado a torcida. Acompanhava, mas de longe, sem ligar muito.',
 
-        "description3":'',
+        "description3":'Até um dia que eu comecei a acompanhar o Casimiro, sim, o streamer, e vi a paixão dele pelo Vasco. Falava: "Mano, como é possível isso? O time que o cara torce simplesmente é o PIOR DO MUNDO".',
         
-        "description4":'',
+        "description4":'Comecei a perceber que, mesmo meu time na pior fase ou na melhor, o que importa é sentir a paixão pelo clube. É algo inexplicável você ir para o estádio, cantar o hino brasileiro e canções da torcida. É algo que é único, arrepia, emociona e torna o momento de estar lá torcendo especial.',
 
-        "description5":'',
+        "description5":'Pelo menos é algo que eu sinto quando estou no estádio. E mesmo perdendo, obviamente fico frustrado e triste, mas sinto que valeu a pena ir torcer, vibrar, gritar, sentir raiva, chorar, entre outras emoções, porque além de me conectar com outras pessoas através de uma instituição, eu sinto que existe amor no mundo.',
 
-        "description6":'',
+        "description6":'Algo que você pode julgar tosco, mas que, na minha filosofia de vida, faz sentido.',
 
         "description7":''
         
@@ -48,24 +48,27 @@ export const dataFake = [
     },
     {
         "id" : '3' ,
-        "photo":"aaa",
-        "title":"saiu novo filme",
+        "photo":"../assets/imgs/jazz.png",
+        "title":"Músicas",
                 
-        "description":'',
+        "description":`Considerações iniciais: não sou um crítico de música, não sei nem o nome para isso, só quero falar sobre o meu gosto. Bom, o que eu mais escuto é algo voltado ao jazz e suas derivações, como rap, trap, rock e o próprio jazz. Todos os artistas tem suas menções honrosas, ou seja mais de uma música que eu queria colocar, só não quis colocar. Enfim, vamos às músicas:`,
 
-        "description1":'',
+        "description1":`“It's a Man's Man's Man's World”, de James Brown. Cara, eu gosto muito da “TRILHA SONORA”, não sei se é beat,  sou leigo, além da performance sensacional do James, inclusive os vídeos dele cantando e dançando a canção são incríveis, a letra foi a última coisa que eu fui ver. Eu só escutava e, um certo dia, pensei sobre o que ela fala. Sei que fala sobre homens, mulheres, carros, trens e criação. Quando fui ver a letra, me encantei mais pela canção, e quase todo dia coloco ela para tocar (ela está tocando agora).`,
 
-        "description2":'',
+        "description2":`"Smooth Criminal”, “Beat It" e "Billie Jean", de Michael Jackson. Bom, é o Michael, não tem muito o que falar, o Rei do Pop, com muitas polêmicas envolvidas em seu nome, mas com músicas SENSACIONAIS. Menção honrosa para "They Don't Care About Us", que ocorre no BRASIL, em um clipe FANTÁSTICO NO OLODUM. Inclusive, há uma entrevista inédita dele com a Glória Maria. Sim, sou um pouco fã do Michael.`,
 
-        "description3":'',
+        "description3":`“The Man Who Sold The World”, de Nirvana. Nirvana, cara, foi algo bem doido. Conheci assim do nada. Lembro que estava cansado de escutar as mesmas coisas e vi um vídeo falando sobre quem era Kurt Cobain. Pensei: "que louco a história desse mano". Do nada, comecei a escutar as músicas e o fato que posso falar é que você vira o Batman do nada. Como assim o Batman? Você fica meio revoltado, para baixo, querendo ser o Kurt. Loucura, eu sei. Lembro que comentei com um amigo que eu curtia Nirvana e ele falou que também,e que parou de escutar porque já estava ficando meio depressivo. Obviamente, era uma brincadeira. Bom, falei de tudo, mas não da música. Essa literalmente eu entendia uma parte da letra, no caso, "We never lost control, you're face to face, with the man who sold the world". Eu me sentia o cara por saber essa parte, enfim.`,
         
-        "description4":'',
+        "description4":`“Azul da cor do mar”, de Tim Maia. SINCERAMENTE, está sendo difícil escolher as músicas. Pô, existe compositor que têm cinco canções que você fala: “TEM QUE TÁ!”. Mas só o Michael terá a exceção de ter três. Bom, o que eu posso falar sobre o Tim é que, como compositor, é FANTÁSTICO NOTA MIL e, como pessoa, NOTA UM MILHÃO. Vejo clipes, entrevistas e o filme dele, e é perceptível que era uma pessoa gente BOAÇA. Em relação à canção, queria fazer menções honrosas para "Não Quero Só Dinheiro", “O Descobridor Dos Sete Mares" e "Ela Partiu", essas 4 são as que eu mais escuto e todas elas são muito boas. "Azul da cor do mar" é especial porque assim que toca, eu já falo "NOSSA SENHORA, QUE MÚSICA!".
+        `,
 
-        "description5":'',
+        "description5":`"Highway to Hell", do AC/DC, nossa senhora, o começo só na guitarra já dá uma puta música para animar e da vontade de tocar a guitarra invisível. Caso você nunca tenha feito isso, está perdendo muito! Cara, realmente não sei o que falar, porque literalmente na minha cabeça, todas as músicas citadas não precisam de explicação, porque são muito boas. Então, irei falar o que eu penso quando toca: dá vontade de cantar e tocar a guitarra invisível. Isso é o que sinto quando toca. Ah, claro, menções honrosas para "Back in Black" e "Thunderstruck". E claro, conheci através do Homem de Ferro, sim, o Tony Sterco.
+        `,
 
-        "description6":'',
+        "description6":`"Queima Minha Pele", de Baco Exu do Blues, “amor, você é como o sol que ilumina o meu dia, mas queima minha pele…”, é sensacional a letra! E depois que você repara nela e o Banco confirma que essa música é sobre ansiedade, pelo menos para mim fica mais especial, porque na época que estava colocando para tocar eu estava muito ansioso. Pô, combina, entende? É incrível! Tem muitas canções do Baco que são simplesmente f*das. E quando conheci o trabalho dele, foi na música "Flamingos", que é incrível. Depois fui explorar o álbum do cara e continua num nível surreal de bom.
+        `,
 
-        "description7":''
+        "description7":`"Urubus", de Matuê ft Derek, som de vibe incrível e indescritível. E o verso que o Matuê manda, puts, é de outro mundo: "Acharam mais uma carcaça, eles querem tudo de graça, nem correria, nem merecimento, a vergonha na arte da caça". É uma que poucas pessoas conhecem, tem uns milhões de visualizações nela, mas quase ninguém lembra, e ela é simplesmente uma das melhores do mano.`
         
     },
     {
